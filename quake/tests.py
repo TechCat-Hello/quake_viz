16c5a7da83826329e8e68ae3f0cdd7f10a4ec22d
TechCat <a.m.techcat@gmail.com>
2025-05-29 07:14:52 +0900

    Add unit test for Earthquake model

--- quake/tests.py
+++ quake/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Earthquake
+from datetime import date
 
-# Create your tests here.
+class EarthquakeModelTest(TestCase):
+    def test_create_earthquake(self):
+        eq = Earthquake.objects.create(date=date(2023, 5, 1), magnitude=5.0, location='Tokyo')
+        self.assertEqual(eq.magnitude, 5.0)
+        self.assertEqual(eq.location, 'Tokyo')
