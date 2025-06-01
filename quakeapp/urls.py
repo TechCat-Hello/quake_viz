abb1a8fe78afb0c028ace9efc1edb1705b613e1c
TechCat <a.m.techcat@gmail.com>
2025-06-01 21:40:59 +0900

    Revert URL routing to restore quake/ prefix

--- quakeapp/urls.py
+++ quakeapp/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda request: redirect('quake/earthquake/')),
+    path('quake/', include('quake.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
     path('', include('quake.urls')),
