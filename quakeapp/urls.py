3b0d4a13a49ff228f0ddbf4b44a9289024742f75
TechCat <a.m.techcat@gmail.com>
2025-06-01 21:31:48 +0900

    Update URL config: remove 'quake/' prefix

--- quakeapp/urls.py
+++ quakeapp/urls.py
@@ -5,11 +5,10 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: redirect('quake/earthquake/')),  # ←これを追加
-    path('quake/', include('quake.urls')),
+    path('', lambda request: redirect('quake/earthquake/')),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
-    path('', include('quake.urls')), 
+    path('', include('quake.urls')),
     path('password_reset/', auth_views.PasswordResetView.as_view(
         template_name='registration/password_reset_form.html'
     ), name='password_reset'),
